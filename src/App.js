61d41d737280722d244ccd4cf1c4e0e13713618c
Guy Ronen <guy.ronen@codility.com>
2022-10-27 20:09:04 +0200

    Abstract Project comments and form

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Create from "./pages/create/Create";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Project from "./pages/project/Project";
+import Account from "./pages/account/Account";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import OnlineUsers from "./components/OnlineUsers/OnlineUsers";
@@ -29,6 +30,10 @@ function App() {
               <Route exact path="/">
                 {!user && <Redirect to="/login" />}
                 {user && <Dashboard />}
+              </Route>
+              <Route exact path="/account">
+                {!user && <Redirect to="/login" />}
+                {user && <Account />}
               </Route>
               <Route path="/create">
                 {!user && <Redirect to="/login" />}
--- src/pages/comments/ProjectComments.js
+++ src/pages/comments/ProjectComments.js
@@ -1,33 +1,12 @@
-import { useState } from "react";
 import Avatar from "../../components/Avatar/Avatar";
-import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import CommentsForm from "./CommentsForm";
 
 export default function ProjectComments({ project }) {
   const { user } = useAuthContext();
-  const [newComment, setNewComment] = useState("");
-  const { updateDocument, response } = useFirestore("projects");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const commentToAdd = {
-      createdBy: user.uid,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      content: newComment,
-      createdAt: timestamp.fromDate(new Date()),
-      id: Math.random(),
-    };
-    await updateDocument(project.id, {
-      comments: [...project.comments, commentToAdd],
-    });
-    if (!response.error) {
-      setNewComment("");
-    }
-  };
+  const { updateDocument } = useFirestore("projects");
 
   const handleCommentDelete = async (id) => {
     await updateDocument(project.id, {
@@ -63,17 +42,7 @@ export default function ProjectComments({ project }) {
             </li>
           ))}
       </ul>
-
-      <form className="add-comment" onSubmit={handleSubmit}>
-        <label>
-          <span>Add new comment:</span>
-          <textarea
-            onChange={(e) => setNewComment(e.target.value)}
-            value={newComment}
-          ></textarea>
-        </label>
-        <button className="btn">Add Comment</button>
-      </form>
+      <CommentsForm project={project} />
     </div>
   );
 }
