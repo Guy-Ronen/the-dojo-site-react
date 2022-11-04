cc8eea96f7b91bf3e27b7e56f3b6a157f034373c
Guy Ronen <guy.ronen@codility.com>
2022-11-04 10:07:48 +0100

    fixed tests

---
+++ src/components/OnlineUsers/__test__/MockOnlineUsers.test.js
@@ -0,0 +1,17 @@
+import { render, screen, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import MockOnlineUsers from "./MockOnlineUsers";
+import { userProps, errorProps } from "../../../test/testUtils";
+
+
+describe("Online Users", () => {
+  it("should have render online users", () => {
+    render(<MockOnlineUsers props={userProps} />);
+    screen.debug();
+  });
+
+  it("should have render error", () => {
+    render(<MockOnlineUsers props={errorProps} />);
+    screen.debug();
+  });
+});
