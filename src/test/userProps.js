4563580dd09dfd81d8b29add8727579ed52990da
Guy Ronen <guy.ronen@codility.com>
2022-11-07 10:01:42 +0100

    modified projectProps and created a user props

---
+++ src/test/userProps.js
@@ -0,0 +1,26 @@
+export const userProps = {
+
+    users: [
+        {
+            online: true,
+            photoUrl: "photoUrl8",
+            displayName: "guy8",
+        }, {
+            online: false,
+            photoUrl: "photoUrl9",
+            displayName: "guy9",
+        },
+        {
+            online: true,
+            photoUrl: "photoUrl10",
+            displayName: "guy10",
+        },
+
+    ],
+    error: null
+}
+
+export const errorProps = {
+    users: null,
+    error: "Could not fetch the documents"
+}
