407efc4706ce704304fc58a4b746299ffd517dfe
Guy Ronen <guy.ronen@codility.com>
2022-12-04 18:30:50 +0100

    abstracted the auth reducer to a saparate file

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,14 @@
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "UPDATE":
+      return { ...state, user: action.payload };
+    case "AUTH_IS_READY":
+      return { user: action.payload, authIsReady: true };
+    default:
+      return state;
+  }
+};
