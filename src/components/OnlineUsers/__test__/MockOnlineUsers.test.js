3e6b1a2d562fdb9d4a3317e8cedbe57e59b770de
Guy Ronen <guy.ronen@codility.com>
2022-12-04 13:03:36 +0100

    added tests to work with IsOnlineIcon

--- src/components/OnlineUsers/__test__/MockOnlineUsers.test.js
+++ src/components/OnlineUsers/__test__/MockOnlineUsers.test.js
@@ -5,12 +5,27 @@ import { userProps, errorProps } from "../../../test/userProps";
 
 describe("Online Users", () => {
   it("should have render online users", () => {
-    render(<MockOnlineUsers props={userProps} />);
-    screen.debug();
+    const {container} = render(<MockOnlineUsers props={userProps} />);
+    const users = container.getElementsByClassName('user-list-item')
+    expect(users).toHaveLength(3)
+  });
+
+
+  it("should have 2 online users", () => {
+    const { container } = render(<MockOnlineUsers props={userProps} />);
+    const users = container.getElementsByClassName("online-user");
+    expect(users).toHaveLength(2);
+  });
+
+  it("should have 1 offline users", () => {
+    const { container } = render(<MockOnlineUsers props={userProps} />);
+    const user=container.getElementsByClassName("offline-user");
+    expect(user).toHaveLength(1);
   });
 
   it("should have render error", () => {
     render(<MockOnlineUsers props={errorProps} />);
-    screen.debug();
+    const error = screen.getByText(/could not fetch the documents/i)  
+    expect(error).toBeInTheDocument()
   });
 });
