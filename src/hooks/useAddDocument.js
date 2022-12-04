efc9fba72f51bcd00c8aa7cbd30e1fbcd9ae3680
Guy Ronen <guy.ronen@codility.com>
2022-12-04 19:09:38 +0100

    abstracted add document logic to custom hook

---
+++ src/hooks/useAddDocument.js
@@ -0,0 +1,45 @@
+import { useReducer, useEffect, useState } from "react";
+
+import { timestamp } from "../firebase/config";
+
+import { firestoreReducer } from "../reducers/FirestroreReducer";
+
+import { dispatchIfNotCancelled } from "../utils/dispatchIfNotCancelled";
+import { getCollectionRef } from "../utils/getCollectionRef";
+import { initialDocumentState } from "../utils/initialDocumentState";
+
+export const useAddDocument = (collection) => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [response, dispatch] = useReducer(
+    firestoreReducer,
+    initialDocumentState
+  );
+
+  // collection ref
+  const ref = getCollectionRef(collection);
+
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const createdAt = timestamp.fromDate(new Date());
+      const addedDocument = await ref.add({ ...doc, createdAt });
+
+      dispatchIfNotCancelled(dispatch, isCancelled, {
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+
+    } catch (err) {
+      dispatchIfNotCancelled(dispatch, isCancelled, {
+        type: "ERROR",
+        payload: err.message,
+      });
+    }
+  };
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, response };
+};
