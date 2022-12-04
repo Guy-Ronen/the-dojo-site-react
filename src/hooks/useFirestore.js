abc9aacfe344493ad1186f9d4b75e427432e7b4b
Guy Ronen <guy.ronen@codility.com>
2022-12-04 18:29:55 +0100

    inserted the reducer in the useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,6 @@
 import { useReducer, useEffect, useState } from "react";
 import { projectFirestore, timestamp } from "../firebase/config";
+import {firestoreReducer} from '../reducers/FirestroreReducer';
 
 let initialState = {
   document: null,
@@ -8,37 +9,6 @@ let initialState = {
   success: null,
 };
 
-const firestoreReducer = (state, action) => {
-  switch (action.type) {
-    case "IS_PENDING":
-      return { isPending: true, document: null, success: false, error: null };
-    case "ADDED_DOCUMENT":
-      return {
-        isPending: false,
-        document: action.payload,
-        success: true,
-        error: null,
-      };
-    case "DELETED_DOCUMENT":
-      return { isPending: false, document: null, success: true, error: null };
-    case "UPDATED_DOCUMENT":
-      return {
-        isPending: false,
-        document: action.payload,
-        success: true,
-        error: null,
-      };
-    case "ERROR":
-      return {
-        isPending: false,
-        document: null,
-        success: false,
-        error: action.payload,
-      };
-    default:
-      return state;
-  }
-};
 
 export const useFirestore = (collection) => {
   const [response, dispatch] = useReducer(firestoreReducer, initialState);
